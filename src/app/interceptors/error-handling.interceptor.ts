0ed7213bfe9368ee71d86c2dea35b0f1579e0359
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2025-04-18 18:58:51 +0300

    end commit

--- src/app/interceptors/error-handling.interceptor.ts
+++ src/app/interceptors/error-handling.interceptor.ts
@@ -2,16 +2,32 @@ import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest } from "@angul
 import { inject } from "@angular/core";
 import { ToastrService } from "ngx-toastr";
 import { catchError, Observable, throwError } from "rxjs";
+import { JwtService } from "../services/jwt/jwt.service";
+import { Router } from "@angular/router";
 
 export function errorHandlingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
 
   const toastrService = inject(ToastrService);
+  const jwtService = inject(JwtService);
+  const router = inject(Router);
 
   return next(req).pipe(catchError((error: HttpErrorResponse) => {
-    const result: { message: string } | undefined = error.error as { message: string } | undefined;
+
+
+    let result: { message: string } | undefined = error.error as { message: string } | undefined;
+
+    if (error.status == 401) {
+      jwtService.deleteJwtInStorage();
+      router.navigate(['/login'])
+      if (!result) {
+        result = {
+          message: 'Oturum süreniz doldu'
+        };
+      }
+    }
 
     if (result) {
-      toastrService.error(result.message, "Hata");
+      toastrService.error(result.message, 'Hata');
     }
 
     return throwError(() => error);
